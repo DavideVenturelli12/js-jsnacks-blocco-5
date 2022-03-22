13f979837e7b65e41cf8a1a19919427a473c5947
Davide Venturelli <dventurelli90@gmail.com>
2022-03-22 17:39:33 +0100

    jsnack 1-2 finiti

--- jsnack 1-2-3/script.js
+++ jsnack 1-2-3/script.js
@@ -3,10 +3,17 @@ console.log('JS OK!');
 
 /* CONSEGNA:
 
+- Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+Calcola quanto pesano tutte le zucchine. [X]
+
 - Crea 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno 
-o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine. [X]
+
+- Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 
 */
+
+// **** JSNACK 1 ****
 
 //creo un array con 10 oggetti (zucchine)
 
@@ -63,10 +70,29 @@ const zucchine = [
     },
 ];
 
+//stampo la tabella con le proprietà delle zucchine
 console.table(zucchine);
 
+//calcolo la somma del peso delle zucchine
+function sommaPeso(zucchine) {
+    let somma = 0;
+    for (let i = 0; i < zucchine.length; i++) {
+        somma += zucchine[i].peso;
+    }
+    return somma
+}
+
+//stampo la somma del peso in g e kg
+console.log(`il peso di tutte le zucchine è: ${sommaPeso(zucchine)} g, ovvero ${sommaPeso(zucchine) / 1000} kg`)
+
+
+
+// **** JSNACK 2 ****
+
+//creo un array per le zucchine < 15cm
 const zucchineCorte = [];
 
+//creo un array per le zucchine > 15cm
 const zucchineLunghe = [];
 
 for (let i = 0; i < zucchine.length; i++) {
@@ -78,5 +104,13 @@ for (let i = 0; i < zucchine.length; i++) {
 }
 
 console.table(zucchineCorte);
+//stampo la somma del peso in g e kg
+console.log(`il peso delle zucchine corte è: ${sommaPeso(zucchineCorte)} g, ovvero ${sommaPeso(zucchineCorte) / 1000} kg`);
 
 console.table(zucchineLunghe);
+//stampo la somma del peso in g e kg
+console.log(`il peso delle zucchine corte è: ${sommaPeso(zucchineLunghe)} g, ovvero ${sommaPeso(zucchineLunghe) / 1000} kg`);
+
+
+
+// **** JSNACK 3 ****
--- jsnack 3/script.js
+++
@@ -1,9 +0,0 @@
-//debug
-console.log('JS OK!');
-
-/* CONSEGNA:
-
-- Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
-
-*/
-
